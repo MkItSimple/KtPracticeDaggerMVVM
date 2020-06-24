7226f816c6bdfb7d719f46a1b95a435b36c42a2b
MkItSimple <choanania1@gmail.com>
2020-06-25 01:10:03 +0800

    p2-DWithApi

--- app/src/main/java/com/mkitsimple/ktpracticedaggermvvm/ui/main/MainViewModel.kt
+++ app/src/main/java/com/mkitsimple/ktpracticedaggermvvm/ui/main/MainViewModel.kt
@@ -1,11 +1,16 @@
 package com.mkitsimple.ktpracticedaggermvvm.ui.main
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mkitsimple.ktpracticedaggermvvm.data.models.PokemonResponse
 import com.mkitsimple.ktpracticedaggermvvm.data.repositories.MyRepository
+import com.mkitsimple.ktpracticedaggermvvm.data.repositories.PokemonRepository
 import com.mkitsimple.ktpracticedaggermvvm.domain.AppUsecase
+import com.mkitsimple.ktpracticedaggermvvm.domain.PokemonUsecase
 import com.mkitsimple.ktpracticedaggermvvm.utils.Coroutines
+import io.reactivex.disposables.CompositeDisposable
 import kotlinx.coroutines.Job
 import javax.inject.Inject
 
@@ -17,7 +22,7 @@ class MainViewModel @Inject constructor() : ViewModel() {
 //        this.repository  = MyRepository()
 //    }
     @Inject
-    lateinit var repository: MyRepository
+    lateinit var usecase: PokemonUsecase
 
     private lateinit var job: Job
 
@@ -27,14 +32,33 @@ class MainViewModel @Inject constructor() : ViewModel() {
 
     fun performLogin(email: String, password: String) {
             job = Coroutines.ioThenMain(
-                { repository.performLogin(email, password) },
+                { usecase.performLogin(email, password) },
                 { _isLoginSuccess.value = it }
                 //_isLoginSuccess.value = repository.performLogin(email, password)
             )
     }
 
+    private val pokemonListMutableLiveData = MutableLiveData<PokemonResponse>()
+
+    fun getPokemonList() {
+        //Log.d("PokemonCalled", "PokemonCalled")
+        if (pokemonListMutableLiveData.value != null) {
+            return
+        }
+        val disposable = usecase.getPokemonList(0)
+            .subscribe {
+                pokemonListMutableLiveData.value = it
+            }
+        compositeDisposable.add(disposable)
+    }
+
+    fun getLivePokemonList() = pokemonListMutableLiveData
+
+    val compositeDisposable = CompositeDisposable()
+
     override fun onCleared() {
         super.onCleared()
         if(::job.isInitialized) job.cancel()
+        compositeDisposable.clear()
     }
 }
